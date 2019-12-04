754abb5e2f9ccd924ccc32c67a080719847b97dc
sahmali <shahmalimh@code.edu.az>
2019-12-04 19:46:37 +0400

    lightbox hazir

--- assets/js/delta.js
+++ assets/js/delta.js
@@ -3,3 +3,29 @@ for (var i = $coverItems.length - 1; i >= 0; i--) {
 
 	$coverItems[i].style.left = 100/11*i+10+'%'
 }
+
+
+
+
+////////lightbox js
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('.see-img-btn')
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('lightactive')
+    const img = document.createElement('img')
+    img.src = image.getAttribute('imagesrc')
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
+})
+
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('lightactive')
+})
